fe6d1b187f5c6facad859833df2af22e91a04af6
Triplecalc <76068667+Triplecalc@users.noreply.github.com>
2021-07-05 14:18:50 +0500

    Add files via upload

---
+++ runscript.js
@@ -0,0 +1,16 @@
+// Created by Mika
+
+alert ("С днём рождения, любимая! Нажми на экран чтобы зазвучала музыка! ❤️🌿✨💖💞🍷🎊🎉🎈🧨🎆🎇🎁🎀🍰🎂🍪🧁🥧🍫🍮☕🍷🥂")
+var a = new Audio("https://dl.dropbox.com/s/ladqsz9w7ybkwqg/Good%20Wishes%20For%20Your%20Birthday%20-%20Birthday%20Song%28M4A_128K%29_1.m4a?dl=0");
+
+window.addEventListener('click',() => {
+a.play();
+});
+
+function start(){
+    document.getElementById("all").innerHTML=" ";
+    document.getElementById("all").style.display="none";
+    document.getElementById("all").style.background="trasparent";
+}
+
+setInterval(start, 5000);
